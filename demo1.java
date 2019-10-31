2503737adea829580c7517ee5a358b37db748ce2
megha97-gc <57207398+megha97-gc@users.noreply.github.com>
2019-10-31 12:45:52 +0530

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,20 @@
+package product;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		
+			{
+				System.out.println(10+20);
+				System.out.println(10+20.5);
+				System.out.println(10+'a');
+				System.out.println('A'+'b');
+				System.out.println('c'+10.0);
+				System.out.println(""+'a');
+				System.out.println('a'+"my");
+				System.out.println(false+"hello");
+			}
+		}
+
+	}
+
